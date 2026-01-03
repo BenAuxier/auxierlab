a0037ba615e657ecf84476ae57d61269def4afb6
BenAuxier <benauxier@gmail.com>
2026-01-03 06:38:52 +0000

    Deploying to gh-pages from @ BenAuxier/auxierlab@edf18256dc5431bdda700b0663c112d3ee402535 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,8 +456,8 @@ ninja.data = [{
           description: "How fungi recognise each other",
           section: "Projects",handler: () => {
               window.location.href = "/auxierlab/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-evolution-of-viral-virulence",
+          title: 'Evolution of viral virulence',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/auxierlab/projects/2_project/";
@@ -497,39 +497,32 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/auxierlab/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
+        id: 'social-bluesky',
+        title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("/auxierlab/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("https://bsky.app/profile/benauxier.bsky.social", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%65%6E.%61%75%78%69%65%72@%77%75%72.%6E%6C", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/auxierlab/feed.xml", "_blank");
+          window.open("https://orcid.org/0000-0002-7743-0610# your ORCID ID", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=qfIv5S0AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
